eedc505bd3b17b3947eb527e8f4bcfd69fe2f0f5
CallMeFilms <lucas546alexander@gmail.com>
2017-01-14 13:40:02 -0700

    Update GameJoinSign.java

--- GameJoinSign.java
+++ GameJoinSign.java
@@ -1,6 +1,7 @@
 package com.thechocolatedev;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -19,6 +20,9 @@ public class GameJoinSign implements Listener{
 		Player player = (Player) event.getPlayer();
 		Block clicked = (Block) event.getClickedBlock();
 		
+		//A test message
+		player.sendMessage("Test");
+		
 		//An if statement checking to see if the clicked block is a sign
 		if(clicked.getType() == Material.SIGN || clicked.getType() == Material.SIGN_POST || clicked.getType() == Material.WALL_SIGN) {
 			
@@ -28,7 +32,7 @@ public class GameJoinSign implements Listener{
 			if(sign.getLine(0).equals("[Join]") && sign.getLine(1).equals("CTF") && sign.getLine(2).equals("Lobby:")) {
 				
 				//Getting the lobby number on the fourth line and setting it as a variable
-				String signThirdLine = sign.getLine(4);
+				String signThirdLine = sign.getLine(3);
 				
 				//Switching the lobby number from a string to a double
 				int lobbyNumber = Integer.parseInt(signThirdLine);
@@ -40,6 +44,14 @@ public class GameJoinSign implements Listener{
 				
 				//Teleport the player to the location
 				player.teleport(lobbyLoc);
+				
+				/* A for that sends a message to every player in the world telling them another player
+				has joined the game and the current player count of that world */
+				for (Player p : Bukkit.getServer().getWorld("Lobby" + lobbyNumber).getPlayers()) {
+					
+				      p.sendMessage(ChatColor.AQUA + "[" + ChatColor.YELLOW + "Player" + ChatColor.DARK_AQUA + "Count" + ChatColor.AQUA + "] " + ChatColor.DARK_AQUA + "+1 " + ChatColor.AQUA + "Another player has joined. " + Bukkit.getServer().getWorld("Lobby" + lobbyNumber).getPlayers().size() + "/10");
+				      
+				}
 				
 			}
 			
