1be8ad366d47a76eb1e9d873a00c3366fc1b4e4e
CallMeFilms <lucas546alexander@gmail.com>
2017-01-16 17:55:49 -0700

    Create GameWait.java

---
+++ GameWait.java
@@ -0,0 +1,111 @@
+package com.thechocolatedev;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.scheduler.BukkitScheduler;
+
+public class GameWait implements Listener{
+	
+	@EventHandler
+	public void onWorldChange(PlayerChangedWorldEvent event) {
+		
+		//Get the player of the event
+		Player player = (Player) event.getPlayer();
+		
+		//Get the player's new world
+		World newWorld = (World) player.getWorld();
+		
+		//Get the amount of players in the player's new world
+		List<Player> players = newWorld.getPlayers();
+		int playerAmount = players.size();
+		
+		if(event.getFrom().equals(Bukkit.getWorld("Lobby"))) {
+			
+			/* A bukkit scheduler set to wait till after the teleportation.
+			Approx time: 2.5 seconds (50 ticks) */
+	        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+	        
+	        scheduler.scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("ChocoPlayerSpawn"), new Runnable() {
+	        	
+	            @Override
+	            public void run() {
+	            	
+	            	/* Each of the following if and else if statements check to see
+	            	if there is a specific number of players in the player's new world */
+	            	if(playerAmount == 1) {
+	    				
+	    				player.sendMessage(ChatColor.RED + "Please wait for more players, to begin the game");
+	    				
+	    			}
+	    			
+	    			else if(playerAmount < 8){
+	    				
+	    				// A for statement telling everyone the status of the wait
+	    				for (Player p : newWorld.getPlayers()) {
+	    					
+	    				      p.sendMessage(ChatColor.RED + "The game needs at least 8 players to begin.");
+	    				      
+	    				}
+	    				
+	    			}
+	    			
+	    			else if(playerAmount == 8){
+	    				
+	    				// A for statement telling everyone the status of the wait
+	    				for (Player p : newWorld.getPlayers()) {
+	    					
+	    				      p.sendMessage(ChatColor.AQUA + "The game will begin in" + ChatColor.DARK_AQUA + "30" + ChatColor.AQUA + "seconds.");
+	    				      
+	    				}
+	    				
+	    			}
+	    			
+	    			else if(playerAmount == 9) {
+	    				
+	    				// A for statement telling everyone the status of the wait
+	    				for (Player p : newWorld.getPlayers()) {
+	    					
+	    				      p.sendMessage(ChatColor.AQUA + "The game will begin in" + ChatColor.DARK_AQUA + "15" + ChatColor.AQUA + "seconds.");
+	    				      
+	    				}
+	    				
+	    			}
+	    			
+	    			else if(playerAmount == 10) {
+	    				
+	    				// A for statement telling everyone the status of the wait
+	    				for (Player p : newWorld.getPlayers()) {
+	    					
+	    				      p.sendMessage(ChatColor.AQUA + "The game will begin in" + ChatColor.DARK_AQUA + "5" + ChatColor.AQUA + "seconds.");
+	    				      
+	    				}
+	    				
+	    			}
+	    			
+	    			else if(playerAmount >= 11) {
+	    				
+	    				//Get the main lobby world and put it in a location and teleport the player to that location
+	    				World mainLobby = (World) Bukkit.getServer().getWorld("Lobby");
+	    				Location mlLoc = new Location(mainLobby, 0, 100, 0);
+	    				player.teleport(mlLoc);
+	    				player.sendMessage("That game is full. Please wait till next round or find another game.");
+	    				
+	    			}
+	            	
+	            }
+	            
+	        }, 50L );
+			
+		}
+		
+	}
+
+}
